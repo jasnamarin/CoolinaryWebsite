32209c1b986c25830ec2c61a8e3f245abe09e9a8
Jasna Marin <jasna.marin.ru99@gmail.com>
2021-06-27 12:56:22 +0200

    Added search recipes

--- coolinary/src/views/RecipesList.vue
+++ coolinary/src/views/RecipesList.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="recipes_list">
-    <h1>Recipes List</h1>
+  <div class="list">
+    <div class="list__container">
+      <div v-on:search.prevent="onSearch" class="list__filters">
+        <div class="list__item">
+            <label for="standard-select">{{dictionary.sort}}</label>
+            <div class="select">
+                <select id="standard-select">
+                    <option value="rating">{{dictionary.rating}}</option>
+                    <option value="level">{{dictionary.level}}</option>
+                </select>
+            </div>
+        </div>
+        <div class="list__item">
+            <Input v-model="search" type="text" />
+            <Button type="submit" :text="dictionary.search" />
+        <div/>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
@@ -8,24 +25,100 @@
 import rsLang from '@/assets/language/recipes/sr.json';
 import engLang from '@/assets/language/recipes/eng.json';
 
+import Button from "@/components/shared/Button.vue";
+import Input from "@/components/shared/Input.vue";
+
 export default {
   name: 'RecipesList',
   props: ["language"],
+  components: {
+      Button,
+      Input
+  },
   computed: {
     dictionary: function () {
       console.log(this.$props.language)
       return this.$props.language === "english" ? engLang : rsLang;
     },
   },
+  methods: {
+    onSearch: function () {
+        console.log()
+        //search();
+      //this.$router.push("/recipe-details");
+    },
+  },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
-    .recipes_list {
-        margin: 7rem auto;
+    select {
+        width: 100%;
+        min-width: 15ch;
+        max-width: 30ch;
+        border: none;
+        padding: 0.25em 0.5em;
+        font-size: 1.25rem;
+        cursor: pointer;
+        line-height: inherit;
+        background-color: #fff;
+        background-image: linear-gradient(to top, #f9f9f9, #fff 33%);
+        align-items: center;
+        outline: none;
+    }
+
+    select::-ms-expand {
+        display: none;
+    }
+
+    .list {
+        margin: 5rem auto;
         width: 100vw;
-        height: 100wh;
+        height: 100vh;
+
+        Button {
+            margin: 0rem 1rem 0rem 1rem;
+            min-width: 150px;
+            min-height: 30px;
+            background-color: gray;
+
+            &:hover {
+                box-shadow: 0 0 5px gray;
+            }
+        }
+
+        Input {
+            &:focus {
+                transition: all 0.3s;
+                box-shadow: 0 0 5px gray;
+                border: 1px solid gray;
+                outline: none;
+            }
+        }
+        
+        &__container {
+            display: flex;
+            justify-content: space-around;
+            padding-top: 40px;
+        }
+
+        &__filters {
+            display: flex;
+            padding: 1rem auto;
+            align-items: center;
+            justify-content: space-between;
+            position: fixed;
+            background: #fff;
+            width: 100%;
+        }
+
+        &__item {
+            position: relative;
+            display: flex;
+            margin-left: 2rem;
+            align-items: center;
+        }
     }
 
 </style>
