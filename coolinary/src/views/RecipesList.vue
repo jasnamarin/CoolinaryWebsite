5cccc9b2a6cd13dd38a68c4c5560f564917138b1
jasnamarin <50246012+jasnamarin@users.noreply.github.com>
2021-06-27 18:44:39 +0200

    Recipe card (#8)
    * fix
    
    * Recipe card
    
    * fix 5 star rating
    
    Co-authored-by: mvvita <markovitiz7@gmail.com>

--- coolinary/src/views/RecipesList.vue
+++ coolinary/src/views/RecipesList.vue
@@ -1,46 +1,65 @@
+
 <template>
   <div class="list">
     <div class="list__container">
       <div v-on:search.prevent="onSearch" class="list__filters">
         <div class="list__item">
-            <label for="standard-select">{{dictionary.sort}}</label>
-            <div class="select">
-                <select id="standard-select">
-                    <option value="rating">{{dictionary.rating}}</option>
-                    <option value="level">{{dictionary.level}}</option>
-                </select>
-            </div>
+          <label for="standard-select">{{ dictionary.sort }}</label>
+          <div class="select">
+            <select id="standard-select">
+              <option value="rating">{{ dictionary.rating }}</option>
+              <option value="level">{{ dictionary.level }}</option>
+            </select>
+          </div>
         </div>
         <div class="list__item">
-            <Input v-model="search" type="text" />
-            <Button type="submit" :text="dictionary.search" />
-        <div/>
+          <Input v-model="search" type="text" />
+          <Button type="submit" :text="dictionary.search" />
+        </div>
+      </div>
+
+      <div class="recipe-card">
+        <RecipeCard rating="3" />
+      </div>
+      <div class="recipe-card">
+        <RecipeCard
+          name="A Recipe"
+          thumbnail="../../assets/images/thumbnails/dessert-thumbnail.jpg"
+          category="dessert"
+          level="3"
+          time="30min"
+        />
+      </div>
+      <div class="recipe-card">
+        <RecipeCard />
       </div>
     </div>
   </div>
-  </div>
 </template>
 
-<script>
-import { getRecipesForLanguageAndType } from '@/utils'
 
-import rsLang from '@/assets/language/recipes/sr.json';
-import engLang from '@/assets/language/recipes/eng.json';
+<script>
+import { getRecipesForLanguageAndType } from "@/utils";
+
+import rsLang from "@/assets/language/recipes/sr.json";
+import engLang from "@/assets/language/recipes/eng.json";
 
 import Button from "@/components/shared/Button.vue";
 import Input from "@/components/shared/Input.vue";
+import RecipeCard from "@/components/shared/RecipeCard.vue";
 
 export default {
-  name: 'RecipesList',
+  name: "RecipesList",
   props: ["language"],
   data() {
     return {
       recipes: [],
-    }
+    };
   },
   components: {
-      Button,
-      Input
+    Button,
+    Input,
+    RecipeCard,
   },
   computed: {
     dictionary: function () {
@@ -49,18 +68,18 @@ export default {
   },
   mounted() {
     const type = this.$route.fullPath.split("/").slice(-1)[0];
-    this.recipes = getRecipesForLanguageAndType(this.$props.language, type)
+    this.recipes = getRecipesForLanguageAndType(this.$props.language, type);
   },
   watch: {
-    language: function(language) {
+    language: function (language) {
       const type = this.$route.fullPath.split("/").slice(-1)[0];
-      this.recipes = getRecipesForLanguageAndType(language, type)
-    }
+      this.recipes = getRecipesForLanguageAndType(language, type);
+    },
   },
   methods: {
     onSearch: function () {
-        console.log()
-        //search();
+      console.log();
+      //search();
       //this.$router.push("/recipe-details");
     },
   },
@@ -68,73 +87,76 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.recipe-card {
+  margin: 2rem auto;
+}
 
-    select {
-        width: 100%;
-        min-width: 15ch;
-        max-width: 30ch;
-        border: none;
-        padding: 0.25em 0.5em;
-        font-size: 1.25rem;
-        cursor: pointer;
-        line-height: inherit;
-        background-color: #fff;
-        background-image: linear-gradient(to top, #f9f9f9, #fff 33%);
-        align-items: center;
-        outline: none;
+select {
+  width: 100%;
+  min-width: 15ch;
+  max-width: 30ch;
+  border: none;
+  padding: 0.25em 0.5em;
+  font-size: 1.25rem;
+  cursor: pointer;
+  line-height: inherit;
+  background-color: #fff;
+  background-image: linear-gradient(to top, #f9f9f9, #fff 33%);
+  align-items: center;
+  outline: none;
+}
+
+select::-ms-expand {
+  display: none;
+}
+
+.list {
+  margin: 5rem auto;
+  width: 100vw;
+  height: 100vh;
+
+  Button {
+    margin: 0rem 1rem 0rem 1rem;
+    min-width: 150px;
+    min-height: 30px;
+    background-color: gray;
+
+    &:hover {
+      box-shadow: 0 0 5px gray;
     }
+  }
 
-    select::-ms-expand {
-        display: none;
+  Input {
+    &:focus {
+      transition: all 0.3s;
+      box-shadow: 0 0 5px gray;
+      border: 1px solid gray;
+      outline: none;
     }
+  }
 
-    .list {
-        margin: 5rem auto;
-        width: 100vw;
-        height: 100vh;
+  &__container {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+  }
 
-        Button {
-            margin: 0rem 1rem 0rem 1rem;
-            min-width: 150px;
-            min-height: 30px;
-            background-color: gray;
+  &__filters {
+    display: flex;
+    padding: 3rem 1rem 1rem 1rem;
+    align-items: center;
+    justify-content: space-between;
+    position: fixed;
+    background: #fff;
+    width: 100%;
+  }
 
-            &:hover {
-                box-shadow: 0 0 5px gray;
-            }
-        }
-
-        Input {
-            &:focus {
-                transition: all 0.3s;
-                box-shadow: 0 0 5px gray;
-                border: 1px solid gray;
-                outline: none;
-            }
-        }
-        
-        &__container {
-            display: flex;
-            justify-content: space-around;
-            padding-top: 40px;
-        }
-
-        &__filters {
-            display: flex;
-            padding: 1rem auto;
-            align-items: center;
-            justify-content: space-between;
-            position: fixed;
-            background: #fff;
-            width: 100%;
-        }
-
-        &__item {
-            position: relative;
-            display: flex;
-            margin-left: 2rem;
-            align-items: center;
-        }
-    }
-
+  &__item {
+    position: relative;
+    display: flex;
+    margin-left: 2rem;
+    align-items: center;
+  }
+}
 </style>
